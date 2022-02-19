bf0933f44fe752a14642bf676b0412385e05b430
Jason <jjmack921@gmail.com>
2022-02-19 09:43:28 -0800

    Added variations of duckParkRedA3 and classes that looped auto methods to test them

---
+++ src/com/acmerobotics/frieghtFrenzy/auto/duckAndPark/duckParkRedA3Time.java
@@ -0,0 +1,56 @@
+package com.acmerobotics.frieghtFrenzy.auto.duckAndPark;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.frieghtFrenzy.robot.ACMERobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.checkerframework.checker.units.qual.A;
+
+@Config
+@Autonomous(group = "duckAndPark")
+public class duckParkRedA3Time extends LinearOpMode {
+
+    public static double distance1 = 24;
+    public static double distance2 = -28;
+    public static double distance3 = -12;
+    public static double distance4 = 28;
+
+    public static double time1 = 2500;
+    public static double time2 = 2000;
+    public static double time3 = 2500;
+    public static double time4 = 2000;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        ACMERobot robot = new ACMERobot(this);
+
+        waitForStart();
+
+        robot.drive.driveStraight(distance1);
+        robot.runForTime((long)time1);
+
+        robot.drive.turnRight(90);
+        robot.runUntil(robot.drive::atTargetAngle);
+
+        robot.drive.driveStraight(distance2);
+        robot.runForTime((long)time2);
+
+        robot.drive.turnLeft(90);
+        robot.runUntil(robot.drive::atTargetAngle);
+
+        robot.drive.driveStraight(distance3);
+        robot.runForTime((long)time3);
+
+        robot.duckWheel.rampUp(90, 25, "red");
+        robot.runUntil(robot.duckWheel::isRampingUpCompleted);
+
+        robot.drive.driveStraight(distance4);
+        robot.runForTime((long)time4);
+
+        robot.drive.stopDrive();
+
+
+    }
+}
